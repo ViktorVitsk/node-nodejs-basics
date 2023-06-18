93cfb1ce26074840756f22239ce7869e61fb6326
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-18 14:46:14 +0300

    feat: implement fn parseEnv in env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,25 @@
-const parseEnv = () => {
-    // Write your code here 
+/**
+ * Parses environment variables that have a specified prefix 
+ *  and outputs them as a formatted string in console.
+ * @param {string} varNamePrefix - The prefix of the environment 
+ *  variable names to parse.
+ */
+const parseEnv = varNamePrefix => {
+    const env = process.env;
+    const envKeysToArray = Object.keys(env);
+    const fullVarsNames = envKeysToArray.filter(key =>
+        key.includes(varNamePrefix)
+    );
+    let result = '';
+    fullVarsNames.forEach((key, i) => {
+        result += `${key}=${env[key]}`;
+        if (fullVarsNames.length - 1 !== i) {
+            result += '; ';
+        }
+    });
+    console.log(result);
 };
 
-parseEnv();
+const PREFIX = 'RSS_';
+
+parseEnv(PREFIX);
