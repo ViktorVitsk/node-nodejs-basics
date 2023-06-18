262cbc9d1318b327cca3636d5f0ef3e5c20f3bd5
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-18 17:39:07 +0300

    feat: implement fn calculateHash in calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import { join } from 'path';
+import { createHash } from 'crypto';
+
+import { pathToFile } from '../utils.js';
+
+/**
+ * Calculate the SHA256 hash for a file and log it to the console as hex
+ * @param {string} relativePath - relative file path.
+ */
+const calculateHash = async relativePath => {
+    const pathToCurrentFile = pathToFile(import.meta.url);
+    const finalPath = join(pathToCurrentFile, relativePath);
+    const data = await fs.readFile(finalPath);
+
+    const hash = createHash('sha256')
+        .update(data)
+        .digest('hex');
+
+    console.log(hash);
 };
 
-await calculateHash();
+const FILE = './files/fileToCalculateHashFor.txt';
+
+await calculateHash(FILE);
