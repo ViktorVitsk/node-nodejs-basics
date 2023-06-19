d260a5611c0a07334ec9281b641cd7e16849ecbb
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-19 19:11:44 +0300

    feat: implement fns in src/wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import { join } from 'path';
+import { pathToFile } from '../utils.js';
+
 const performCalculations = async () => {
-    // Write your code here
+    const pathToCurDir = pathToFile(import.meta.url);
+    const pathToWorker = join(pathToCurDir, 'worker.js');
+
+    const numOfThreads = os.availableParallelism();
+    let startNum = 10;
+
+    const workers = [];
+
+    for (let i = 0; i < numOfThreads; i++) {
+        const worker = new Worker(pathToWorker, { workerData: startNum++ });
+        workers.push(worker);
+    }
+
+    const workersResults = await Promise.allSettled(
+        workers.map(
+            worker =>
+                new Promise((resolve, reject) => {
+                    worker.on('message', msg => resolve(msg));
+                    worker.on('error', msg => reject(msg));
+                })
+        )
+    );
+
+    const finishResults = workersResults.map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null,
+    }));
+
+    console.log(finishResults);
 };
 
 await performCalculations();
