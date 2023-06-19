36b65a9987d3c727ad83cf9ff072cc2af255761d
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-19 20:05:22 +0300

    feat: implement fn cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'child_process';
+import { join } from 'path';
+import { pathToFile } from '../utils.js';
+
+const spawnChildProcess = async args => {
+    const pathToCurDir = pathToFile(import.meta.url);
+    const pathToCP = join(pathToCurDir, 'files', 'script.js');
+
+    // spawns child process
+    const cp = spawn('node', [pathToCP, ...args]);
+
+    // child process stdin receives input from master process stdin
+    process.stdin.pipe(cp.stdin);
+    // child process stdout sends data to master process stdout
+    cp.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+const ARGS = ['rs', 'school'];
+
+spawnChildProcess(ARGS);
