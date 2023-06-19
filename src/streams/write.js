f58bfde37b8dc00f9badc83280d2c8980db68135
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-19 14:39:37 +0300

    feat: implement fn write in streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
-const write = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { join } from 'path';
+import { pathToFile } from '../utils.js';
+
+/**
+ * writes process.stdin data into file fileToWrite.txt content 
+ *  using Writable Stream
+ * @param {string} folderName - The name of the folder containing the file.
+ * @param {string} fileName - The name of the file to write.
+ */
+const write = async (folderName, fileName) => {
+    const pathToCurrentFile = pathToFile(import.meta.url);
+    const finalPath = join(pathToCurrentFile, folderName, fileName);
+
+    const writeStream = fs.createWriteStream(finalPath);
+
+    process.stdin.pipe(writeStream);
 };
 
-await write();
+const FOLDER = 'files';
+const FILE = 'fileToWrite.txt';
+
+await write(FOLDER, FILE);
