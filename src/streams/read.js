ee0c4400e091e8a800fbc47ea5826aae20230050
Viktor Stoianov <viktor.web.frontend@gmail.com>
2023-06-19 14:30:54 +0300

    feat: implement fn read in streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
-const read = async () => {
-    // Write your code here 
+import fs from 'fs';
+import { join } from 'path';
+import { pathToFile } from '../utils.js';
+
+/**
+ * Reads the content of the specified file using a Readable Stream 
+ *  and prints it to process.stdout.
+ * @param {string} folderName - The name of the folder containing the file.
+ * @param {string} fileName - The name of the file to read.
+ */
+const read = async (folderName, fileName) => {
+    const pathToCurrentFile = pathToFile(import.meta.url);
+    const finalPath = join(pathToCurrentFile, folderName, fileName);
+
+    const readStream = fs.createReadStream(finalPath);
+
+    readStream.pipe(process.stdout);
 };
 
-await read();
+const FOLDER = 'files';
+const FILE = 'fileToRead.txt';
+
+await read(FOLDER, FILE);
